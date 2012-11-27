2fc484e4dfafac40aa386479beac0fbeb2952f9e
Dawid Roszkiewicz <droszkiewicz@p110-9.labpk.inf.ug.edu.pl>
2012-11-27 13:14:15 +0100

    LMAOL

--- jakisplik2.c
+++ jakisplik2.c
@@ -4,7 +4,7 @@ asgd
 adsg
 asd
 jedna osoba z zespołu daje dostęp pozostałym do swojego repozytorium ( -> admin -> collaborators),
-wszyscy robią kopie lokalne, do edycji,
+wszyscy robią kopie lokalne, do edycji, ANDRZEJ GOŁOTA!
 jedna osoba z zespołu dodaje jakiś dłuższy - 10 linii - kod w C, wszyscy synchronizują repozytorium tak, aby mieli ten plik,
 następnie każda osoba z zespołu zmienia jedną linię kodu, ale każdy inną, oddalone od siebie o conajmniej 2 linie,
 wszyscy eksportują swoje zmiany, i ściąg
