b5773548d79062b4b2a751294466b62a6f41ddf2
Katarzyna Ścibek <kscibek@p110-10.labpk.inf.ug.edu.pl>
2012-11-27 13:11:17 +0100

    kgbwdvsbgsjbvsdklui

--- jakisplik2.c
+++ jakisplik2.c
@@ -3,3 +3,8 @@ g
 asgd
 adsg
 asd
+jedna osoba z zespołu daje dostęp pozostałym do swojego repozytorium ( -> admin -> collaborators),
+wszyscy robią kopie lokalne, do edycji,
+jedna osoba z zespołu dodaje jakiś dłuższy - 10 linii - kod w C, wszyscy synchronizują repozytorium tak, aby mieli ten plik,
+następnie każda osoba z zespołu zmienia jedną linię kodu, ale każdy inną, oddalone od siebie o conajmniej 2 linie,
+wszyscy eksportują swoje zmiany, i ściąg
