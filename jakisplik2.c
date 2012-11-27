aaef491c103ccd489e58b45258fb701f15c6bde1
Jan Świątek <jswiatek@p110-11.labpk.inf.ug.edu.pl>
2012-11-27 13:21:09 +0100

    Konlikt byll

--- jakisplik2.c
+++ jakisplik2.c
@@ -6,10 +6,5 @@ asd
 jedna osoba z zespołu daje dostęp pozostałym do swojego repozytorium ( -> admin -> collaborators),
 wszyscy robią kopie lokalne, do edycji, ANDRZEJ GOŁOTA!
 jedna osoba z zespołu dodaje jakiś dłuższy - 10 linii - kod w C, wszyscy synchronizują repozytorium tak, aby mieli ten plik,
-<<<<<<< HEAD
 następnie każda osoba z zespołu zmienia jedną linię kodu, ale każdy inną, oddalone od siebie o conajmniej 2 linie,
 ZMIEANIAM OSTATNIA LINIJKE  :)-<-<
-=======
-następnie :) żadna osoba z zespołu nie zmieni jednej linii kodu, ale każdy inną, oddalone od siebie o conajmniej 2 linie,
-wszyscy eksportują swoje zmiany, i ściąg
->>>>>>> 24dc5a3d11d038a1500bc7d3a445707472d3ca64
