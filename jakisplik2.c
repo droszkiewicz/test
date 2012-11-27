02e7da5a8e242d46d200503f40e4687d3287b185
Katarzyna Ścibek <kscibek@p110-10.labpk.inf.ug.edu.pl>
2012-11-27 13:14:50 +0100

    trzeba to zmienic

--- jakisplik2.c
+++ jakisplik2.c
@@ -6,5 +6,5 @@ asd
 jedna osoba z zespołu daje dostęp pozostałym do swojego repozytorium ( -> admin -> collaborators),
 wszyscy robią kopie lokalne, do edycji,
 jedna osoba z zespołu dodaje jakiś dłuższy - 10 linii - kod w C, wszyscy synchronizują repozytorium tak, aby mieli ten plik,
-następnie każda osoba z zespołu zmienia jedną linię kodu, ale każdy inną, oddalone od siebie o conajmniej 2 linie,
+następnie :) żadna osoba z zespołu nie zmieni jednej linii kodu, ale każdy inną, oddalone od siebie o conajmniej 2 linie,
 wszyscy eksportują swoje zmiany, i ściąg
